b33bff0fd8eae5250a25cc4e55c00484d8231394
Domnator939131 <75163257+Domnator939131@users.noreply.github.com>
2021-01-20 12:46:16 +0400

    mongo

--- Util/mongo.js
+++ Util/mongo.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const  mongoPath = 'mongodb+srv://Hthere:I2emmWGteM8kpm5s@cluster0.epybr.mongodb.net/Cluster0?retryWrites=true&w=majority'
+const  mongoPath = 'mongodb+srv://Discordbot-Owner:BhXaZosCY6OYbHui@mongodb-discord.oejgy.mongodb.net/MongoDB-Discord?retryWrites=true&w=majority'
 
 module.exports = async () => {
   await mongoose.connect(mongoPath, {
